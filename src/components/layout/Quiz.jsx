7940f244a8c83d24be76b1198cfa7b2aace8f54d
auleki <kodagiwa@gmail.com>
2020-12-15 11:44:46 +0100

    added questions and quiz logic

--- src/components/layout/Quiz.jsx
+++ src/components/layout/Quiz.jsx
@@ -1,70 +1,70 @@
+import { current } from 'immer';
 import React, { useState } from 'react'
-import { QuizPage, QuizBox } from "../StyledComponents";
+import { QuizPage, QuizBox, questionList } from "../StyledComponents";
+
+
+const ScoreView = ({ score }) => {
+  return (
+    <QuizBox>
+      <div className="quiz-title">
+        <h3>Quiz Score</h3>
+        <p>JEAN CLAUDE</p>
+      </div>
+        <h3>{score || 9}</h3>
+    </QuizBox>
+  )
+}
+
 
 const Quiz = () => {
   const [score, setScore] = useState(0)
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [showScore, setShowScore] = useState(false)
+  const [currentQuestion, setCurrentQuestion] = useState(3)
+  const [showScore, setShowScore] = useState(true)
+  const [questions, setQuestions] = useState(questionList)
+  const [limit, setLimit] = useState(5)
+  const [attempt, setAttempt] = useState(1)
 
-  const questions = [
-    {
-      questionText: "__________ is a general term referring to actual objects related to the human body?",
-      answerOptions: [
-        { answerText: 'costume', isCorrect: false },
-        { answerText: 'wardrobe', isCorrect: false },
-        { answerText: 'clothing', isCorrect: true },
-        { answerText: 'wear ability', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'Who is CEO of Tesla?',
-      answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Elon Musk', isCorrect: true },
-        { answerText: 'Bill Gates', isCorrect: false },
-        { answerText: 'Tony Stark', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'The iPhone was created by which company?',
-      answerOptions: [
-        { answerText: 'Apple', isCorrect: true },
-        { answerText: 'Intel', isCorrect: false },
-        { answerText: 'Amazon', isCorrect: false },
-        { answerText: 'Microsoft', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'How many Harry Potter books are there? But then there were no books from Hitler',
-      answerOptions: [
-        { answerText: '1', isCorrect: false },
-        { answerText: '4', isCorrect: false },
-        { answerText: '6', isCorrect: false },
-        { answerText: '7', isCorrect: true },
-      ],
-    },
-  ];
+  const optionHandler = (isCorrect) => {
+    if (isCorrect) setScore(score + 1)
+    const nextQuestion = currentQuestion + 1
+    if (questions.length > nextQuestion && limit !== attempt) {
+      setCurrentQuestion(nextQuestion)
+      setAttempt(attempt + 1)
+    } else {
+      setShowScore(true)
+    }
+  }
 
 
   return (
     <QuizPage>
-      <QuizBox>
-        <div className="quiz-title">
-          <p>BLESSING SMITH</p>
-          <p className="bold">1/5 Questions </p>
-        </div>
-        <div className="row">
-          <p>6 ANSWERS LEFT</p>
-          <p className="bold">20s Remaining</p>
-        </div>
-        <div className="question">
-          <p>{questions[currentQuestion].questionText}</p>
-        </div>
-        <div className="options">
-          {questions[currentQuestion].answerOptions.map((option, i) => (
-            <button>{option.answerText}</button>))}
-        </div>
-      </QuizBox>
+      {showScore
+        ? <ScoreView score={score} />
+        : (
+          <QuizBox>
+            <div className="quiz-title">
+              <p>BLESSING SMITH</p>
+              <p className="bold">{attempt}/{limit} Questions </p>
+            </div>
+            <div className="row">
+              <p>SCORE: <span className="bold">{score}</span></p>
+              {/* <p>6 ANSWERS LEFT</p> */}
+              <p className="bold">20s Remaining</p>
+            </div>
+            <div className="question">
+              <p>{questions[currentQuestion].questionText}</p>
+            </div>
+            <div className="options">
+              {questions[currentQuestion].answerOptions.map((option, i) => (
+                <button onClick={() => optionHandler(option.isCorrect)}>
+                  {option.answerText}
+                  {option.isCorrect ? " R" : ""}
+                </button>))}
+            </div>
+          </QuizBox>
+        )
+      }
+
     </QuizPage>
   )
 }
